c9d7031c0854635b0c4c1a49d9f25c82b970191d
zhiyingzzhou <zhiyingzzhou@gmail.com>
2019-04-23 11:04:16 +0800

    fix template

--- template.js
+++ template.js
@@ -2,7 +2,7 @@ module.exports = function (declareFilePath, services) {
     return `
 /// <reference path="${declareFilePath}" />
 const config = require('./config');
-const request = require('./ajax');
+const request = require('./request');
 const gateway = require('./gateway');
 
 const Service = {
